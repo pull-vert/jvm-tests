90e3a072094f982327eeb937c2ac497e6a4b15a3
pull-vert <frederic.montariol@gmail.com>
2018-11-27 00:49:22 +0100

    example6 : start multiple inheritance use case

---
+++ src/main/java/jvm/java8/constructor/example6/multipleInheritance/Quadrilateral.java
@@ -0,0 +1,14 @@
+/*
+ interface Quadrilateral {
+   api func area() -> Int
+ }
+*/
+
+package jvm.java8.constructor.example6.multipleInheritance;
+
+import internal.annotations.NotNull;
+
+public interface Quadrilateral {
+    @NotNull
+    public int area();
+}
